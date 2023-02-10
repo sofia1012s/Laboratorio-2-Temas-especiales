85908a720643a810227d448c9f050029a7c3f9c0
Sofía Salguero <salguerosofia.1012@gmail.com>
2023-02-10 14:38:12 -0600

    Update interfaz.py
    Funciona selección de puerto serial y recibir datos

--- interfaz.py
+++ interfaz.py
@@ -1,36 +1,57 @@
-"""
-# Importing Libraries
-import serial
-import time
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
-while True:
-    num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value) # printing the value
-"""
-
 #Librerias
-import serial 
+import serial
+import time 
+import serial.tools.list_ports
 
-#Abriendo puerto serial
-try:
-    arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
-except:
-    print("Check port")
+# ---------------- Variables ----------------
+data =[]    #Lista para datos de arduino
+ports = []  #Lista para puertos COM disponibles
+comando = "Datos" + '\n' # Comando para enviar a arduino
 
-#Variables
-    rawdata = []
-    count = 0
+# ---------------- Funciones ----------------
+# Funcion para buscar puertos seriales y definir uno
+def puerto():
+    #Buscando puertos seriales
+    for port in serial.tools.list_ports.comports():
+        ports.append(port.name)
+    print("Lista de puertos:")
+    for i, item in enumerate(ports,1):
+        print(i, '. ' + item, sep='',end='')
+        
+    #Seleccionando puerto
+    input_puerto = input("\nEscriba el número del puerto que desea: ")
+    puerto = str(ports[int(input_puerto)-1])
+    
+    #Conexión serial
+    s = serial.Serial(port = puerto,baudrate=115200)
+    time.sleep(1) #Al conectar el puerto serial, debemos esperar un tiempo para que empiece la comunicación
+    return s
+    
+    
+# Funcion para recibir los datos del puerto serial
+def in_puerto(s):
+    for i in range(3):              #Queremos temperatura, humedad y si está oscuro o no
+        b = s.readline()       # read a byte string
+        string_n = b.decode()       # decode byte string into Unicode  
+        string = string_n.rstrip()  # remove \n and \r
+        flt = float(string)         # convert string to float
+        data.append(flt)            # add to the end of data list
+        time.sleep(0.1)             # wait (sleep) 0.1 seconds
+    
+    print (data)
+    
+def out_puerto(s):
+    s.write(string.encode())
 
-#Recibiendo datos
 
-while count < 5:
-    rawdata.append(str(arduino.readline()))
-    count+=1
+# ---------------- Código Main ----------------
+puerto_serial = puerto()
+""""
+datos = input("Desea enviar datos?: " )
+if datos == 1:
+    out_puerto(puerto_serial)
 
-print(rawdata)
+
+"""
+
+in_puerto(puerto_serial)
