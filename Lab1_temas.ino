c661d487e0bf23e6a33feb4974427b6a96f0be33
Sofía Salguero <salguerosofia.1012@gmail.com>
2023-02-10 15:59:56 -0600

    Update Lab1_temas.ino
    Laboratorio 2 en clase

--- Lab1_temas.ino
+++ Lab1_temas.ino
@@ -19,46 +19,75 @@ float temperatura = 0; //Temperatura tomada por el sensor
 const int temp_maxima = 30; //Temperatura máxima para activar a alarma
 float humedad = 0; //Humedad tomada por el sensor
 int Duty = 0; //Ciclo de trabajo para encender bombillo 
-int sampleTime = 1000; //Tiempo de muestreo en ms
+int sampleTime = 0; //Tiempo de muestreo en ms
 unsigned long sample_start; //Tiempo que ha transcurrido
 unsigned long sample_current; //Tiempo que ha transcurrido
 unsigned long sample_total; //Tiempo entre currentTime y startTime
+bool Serial_stringComplete = false; //Bandera para enviar datos
+String Serial_inputString = ""; //String que debe recibirse del serial
 
 //*************** Set UP ********************
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Iniciando");
   pinMode( bombillo, OUTPUT ); //bombillo inteligente
   pinMode( alarma, OUTPUT ); //alarma
   pinMode( pResistor, INPUT ); //Fotoresistor
   dht.begin(); //Sensor
-  Serial.begin(115200); 
-
   sample_start = millis();
 }
 
 
 //*************** Loop ********************
-void loop() {
-  //Tomar tiempo actual
-  sample_current = millis();
+void loop() { 
+  serialEvent();
 
-  //Tomar cuanto tiempo ha transcurrido desde el último muestreo
-  sample_total = sample_current - sample_start;
-
-  if (sample_total >= sampleTime)
-  {
-    leerFotoresistor();
-    tomarTempHumedad();
-    envioSerial();
-    sample_start = millis();
+  if(Serial_stringComplete){
+    analyzeSensorString();
+    Serial_inputString = "";
+    Serial_stringComplete = false;
   }
+
+  tomarDatos();
   
 }
 
 
 //*************** Funciones ********************
 
+
+//*************** Función para leer nueva data de entrada ********************
+void serialEvent(){
+  while(Serial.available()>0){
+    char inChar = (char)Serial.read();
+    Serial_inputString += inChar;
+    if ((byte(inChar) == 10)){ //Solo lo puede leer como byte el enter, no como \n
+      Serial_stringComplete = true;
+    }      
+  }
+}
+
+
+//*************** Función para buscar bandera true ********************
+void analyzeSensorString(){
+  if(Serial_inputString.indexOf("uno")!= -1){
+    sampleTime = 1000;
+    Serial.println("1 segundo");
+    }
+
+  else if (Serial_inputString.indexOf("cinco")!= -1){
+    sampleTime = 5000;
+    Serial.println("5 segundos");
+  }
+
+    else if (Serial_inputString.indexOf("diez")!= -1){
+    sampleTime = 10000;
+    Serial.println("10 segundos");
+  }
+}
+
 //*************** Funcion para tomar temperatura y humedad ********************
-void tomarTempHumedad(){
+void tomarTempHumedad(){  
   temperatura = dht.readTemperature();
   humedad = dht.readHumidity();
   if(temperatura >= temp_maxima){
@@ -88,4 +117,23 @@ void envioSerial(){
   Serial.println(temperatura);
   Serial.println(humedad);
   Serial.println(oscurecio);
+}
+
+void tomarDatos(){
+    //Tomar tiempo actual
+  sample_current = millis();
+
+  //Tomar cuanto tiempo ha transcurrido desde el último muestreo
+  sample_total = sample_current - sample_start;
+
+  if (sampleTime != 0){
+    if (sample_total >= sampleTime)
+    {
+      leerFotoresistor();
+      tomarTempHumedad();
+      envioSerial();
+      sample_start = millis();
+    }
+  }
+
 }
